068402feb3c6cb5e4d7cbe6da5eb4eab8ce44595
steven <stef.meindl1@gmail.com>
2017-05-09 10:10:33 +0200

    AiRelations aufgeräumt ;)

--- src/Data/AiRelations.java
+++ src/Data/AiRelations.java
@@ -16,37 +16,45 @@ import java.util.List;
 public class AiRelations {
 
     //talk counter mit jeder ai
+    //Sympatie mit jeder ai, standart(neutral)500, max 1000
     private List<Ai> ais = new ArrayList();
     private List<Integer> talkList = new ArrayList();
     private List<Integer> sympList = new ArrayList();
-    //o boi los gehts
+    private List<Integer> knowList = new ArrayList();
     
     public void setAis(ArrayList ais) {
         this.ais = ais;
 
         for (int i = talkList.size(); i < ais.size(); i++) {
             talkList.add(0);
+            sympList.add(500);
         }
 
     }
 
-    public void talk(Ai ai) {
-        int index = 0;
-        for (int i = 0; i < ais.size(); i++) {
-            if (ais.get(i).equals(ai)) {
-                index = i;
-            }
-        }
-        talkList.add(index, talkList.get(index)+1);
+    public void talk(Ai ai) {      
+        talkList.add(searchIndex(ai), talkList.get(searchIndex(ai))+1);
+    }
+    
+    public void plusSymp(Ai ai, int i){
+        sympList.add(searchIndex(ai), sympList.get(searchIndex(ai))+i);
     }
     
     public int getTalk(Ai ai){
-        int index = 0;
-        for (int i = 0; i< ais.size(); i++){
-            index = i;
-        }
-        return talkList.get(index);     
+        return talkList.get(searchIndex(ai));     
+    }
+    
+    public int getSymp(Ai ai){
+        return sympList.get(searchIndex(ai));
     }
 
-
+    private int searchIndex(Ai ai){
+        for(int i = 0; i < ais.size(); i++){
+            if(ais.get(i).equals(ai)){
+                return i;
+            }
+        }
+        return 0;
+    }
+    
 }
